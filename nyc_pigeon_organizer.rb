5267633845f35ca8a07f462119a799b806ce4abe
Gabriel Hicks <gabrielhicks@mail.com>
2020-08-08 19:24:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -32,7 +32,7 @@ def nyc_pigeon_organizer(data)
         end
         
         if !final_array[name][key]
-          final_array[name][key] = value.collect
+          final_array[name][key] = name.collect
           binding.pry
         end
         
