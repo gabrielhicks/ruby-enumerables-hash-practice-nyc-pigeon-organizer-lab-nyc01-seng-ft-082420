35a2dd9e2a725212c56e3d72be417f2d4cda4876
Gabriel Hicks <gabrielhicks@mail.com>
2020-08-08 19:09:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -22,7 +22,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   
   data.each_with_object({}) do |(key, value), final_array|
-    value.each do |key, value|
+    value.each do |inner_key, names|
       binding.pry
     end
   end
