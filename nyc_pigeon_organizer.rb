700f6109a7dc0b193d5b1965e80a326b6316ba74
Gabriel Hicks <gabrielhicks@mail.com>
2020-08-08 19:11:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -24,7 +24,7 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
       names.each do |name|
-        if 
+        if !final_array[name]
       end
     end
   end
